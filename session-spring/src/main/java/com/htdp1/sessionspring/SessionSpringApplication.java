f8005885536ee56e4a127ffbedd8d051b2033b91
h-soon.kim <noisonnoiton@gmail.com>
2021-02-22 15:19:38 +0900

    spring-session init

---
+++ session-spring/src/main/java/com/htdp1/sessionspring/SessionSpringApplication.java
@@ -0,0 +1,13 @@
+package com.htdp1.sessionspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SessionSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SessionSpringApplication.class, args);
+	}
+
+}
---
+++ session-spring/src/test/java/com/htdp1/sessionspring/SessionSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.htdp1.sessionspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SessionSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
