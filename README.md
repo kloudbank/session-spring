b9b26a93b9d4e1a1c95fde7a9bf2a8c4ab45bf98
jaemyeong.lee <jaemyeong.lee@10.250.186.108>
2021-02-24 10:02:34 +0900

    application dev/prd yaml 추

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # session-spring
 session architecture sample application using springboot
+
+
+$ mvn package
+$ docker build -t session-spring .
+$ docker tag session-spring htdp1/session-spring:lastest
+$ docker tag session-spring htdp1/session-spring:v2
+$ docker push htdp1/session-spring:v2
--- src/main/java/com/htdp1/sessionspring/demo/controller/HomeController.java
+++ src/main/java/com/htdp1/sessionspring/demo/controller/HomeController.java
@@ -1,14 +1,27 @@
 package com.htdp1.sessionspring.demo.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HomeController {
 
-    @RequestMapping("/")
-    public String index() {
-        return "Bonjour, Spring,,,";
-    }
-    
+	@Value("${application.name}")
+	public String applicationName;
+
+	@GetMapping("/")
+	public String index() {
+		return "Bonjour, Spring,,,";
+	}
+
+	@GetMapping("/session")
+	@ResponseBody
+	public String index(HttpSession session) {
+		session.setAttribute("applicationName", applicationName);
+		return session.getId() + "\nHello " + session.getAttribute("applicationName");
+	}
 }
