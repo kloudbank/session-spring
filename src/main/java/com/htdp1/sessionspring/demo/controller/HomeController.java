f0e74682dc96033d1e9b474214a12189be9daf21
jaemyeong.lee <robin022@naver.com>
2021-02-24 14:20:49 +0900

    modify application.name to spring.

--- src/main/java/com/htdp1/sessionspring/demo/controller/HomeController.java
+++ src/main/java/com/htdp1/sessionspring/demo/controller/HomeController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HomeController {
 
-	@Value("${application.name}")
+	@Value("${spring.application.name}")
 	public String applicationName;
 
 	@GetMapping("/")
